2a6a5f0e9d9295cfcc96e405491dcc1760b7e8a7
Edwin Velasquez Jimenez <liontumbler@gmail.com>
2024-04-12 10:02:55 -0500

    se grega mas componentes

---
+++ src/components/Modal.vue
@@ -0,0 +1,70 @@
+<template>
+    <div v-if="mostrar" class="modal fade show modal__general" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalDefaultLabel">{{ titulo }}</h5>
+                    <button type="button" class="btn-close" aria-label="Close" @click="cerraModal()"></button>
+                </div>
+                <div class="modal-body">
+                    <slot></slot>
+                </div>
+                <div class="modal-footer">
+                    <button v-if="visibleBtnCerrar" type="button" class="btn btn-secondary" data-bs-dismiss="modal" @click="cerraModal()">{{ msjBtnCerrar }}</button>
+                    <button v-if="visibleBtnContinuar" type="button" class="btn btn-primary" @click="$emit('continuar')">{{ msjBtnContinuar }}</button>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name:'Modal',
+    props :{
+        titulo:{
+            type: String,
+            default: ''
+        },
+        msjBtnCerrar:{
+            type: String,
+            default: 'Cerrar'
+        },
+        msjBtnContinuar:{
+            type: String,
+            default: 'Continuar'
+        },
+        visibleBtnCerrar:{
+            type: Boolean,
+            default: false
+        },
+        visibleBtnContinuar:{
+            type: Boolean,
+            default: false
+        }
+    },
+    data() {
+        return {
+            mostrar: false,
+            isForgotPasswordModal: false,
+        };
+    },
+    methods:{
+        cerraModal() {
+            this.hide();
+            this.$emit('cerrar')
+        },
+        hide() {
+            this.mostrar = false;
+        },
+        show() {
+            this.mostrar = true;
+        },
+    }
+}
+</script>
+<style>
+.modal__general{
+    display: block;
+    background: #00000091;
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -2,39 +2,8 @@
   <div class="bg-body-tertiary">
     <div class="container">
       <div class="row">
-        <div class="col-sm-12 col-md-6 col-lg-4 col-xxl-3 mt-3">
-          <CardProduct :product="{
-            //img: 'https://avatars.githubusercontent.com/u/60611243?v=4',
-            name: 'EDWIN',
-            description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-            stock: 0,
-            reference: 'asd1234',
-            price: 1000,
-          }" />
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-4 col-xxl-3 mt-3">
-          <CardProduct :product="{
-            //img: 'https://avatars.githubusercontent.com/u/60611243?v=4',
-            name: 'title',
-            description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-            stock: 10,
-            reference: 'asd1234',
-            price: 1000,
-          }" />
-        </div>
-        <div class="col-sm-12 col-md-6 col-lg-4 col-xxl-3 mt-3">
-          <CardProduct :product="{
-            img: 'https://avatars.githubusercontent.com/u/60611243?v=4',
-            name: 'title',
-            description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
-            stock: 10,
-            reference: 'asd1234',
-            price: 1000,
-          }" />
-        </div>
-        
-        <div class="col-sm-12 col-md-6 col-lg-4 col-xxl-3 mt-3">
-          <CardProduct></CardProduct>
+        <div class="col-sm-12 col-md-6 col-lg-4 col-xxl-3 mt-3" v-for="(product, index) in products" :key="index">
+          <CardProduct :product="product"></CardProduct>
         </div>
         <div class="col-sm-12 col-md-12 col-lg-12 col-xxl-12 mt-3">
           <CardUser></CardUser>
@@ -42,21 +11,60 @@
       </div>
     </div>
 
-    <Table
-      :data="data"
-      :columns="columns"
-    ></Table>
+    <!--Select-->
+    <div class="mb-3">
+      <label for="select" class="form-label">Disabled select menu</label>
+      <v-select class="form-control" placeholder="Selecione opción" id="select"
+        v-model="select"
+        @option:selected="metoxxx"
+        :options="[
+          { label: 'One', id: 1, img: 'https://avatars.githubusercontent.com/u/60611243?v=4' },
+          { label: 'Two', id: 2, img: 'https://avatars.githubusercontent.com/u/60611243?v=4' },
+          { label: 'THREE', id: 3, img: 'https://avatars.githubusercontent.com/u/60611243?v=4' },
+        ]">
+
+        <template v-slot:option="option">
+          <div class="custom-option row">
+            <div class="col-6">
+              <span class="text-large">{{ option.label }}</span>
+            </div>
+            <div class="col-6">
+              <img :src="option.img" alt="Option Image" class="option-image" width="40"/>
+            </div>
+          </div>
+        </template>
+
+        <template v-slot:no-options="{ search, searching }">
+          <template v-if="searching">
+            No hay resultados con <em>{{ search }}</em>
+          </template>
+        </template>
+      </v-select>
+    </div>
+    <!--Select-->
+
+    <!--fechas range auto-range="5"-->
+    <div>
+      <VueDatePicker class="justify-content-center"
+        v-model="date"
+        :day-names="['Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sá', 'Do']"
+        :max-date="new Date('12/31/2024')"
+        :min-date="Date.now()"
+        inline auto-apply vertical 
+      ></VueDatePicker>
+    </div>
+
+    <Table :data="data" :columns="columns" @editar="() => {metoxxx()}"></Table>
+
+    <modal ref="modal" :visibleBtnContinuar="true">
+        <p>msg</p>
+    </modal>
 
     <!--Login
       @login="login"
     /-->
-
-    
   </div>
-  <Loading 
-    class="z-3 w-100 h-100 position-absolute top-0 start-0 bg-dark text-white opacity-50"
-    :visible="isLoading"
-  />
+  <Loading class="z-3 w-100 h-100 position-absolute top-0 start-0 bg-dark text-white opacity-50" :visible="isLoading" />
 </template>
 
 <script>
@@ -70,11 +78,15 @@
 //console.log('env', import.meta.env);
 
 import bootstrap from 'bootstrap/dist/js/bootstrap.bundle.js';//-
+import vSelect from 'vue-select';
+import VueDatePicker from '@vuepic/vue-datepicker';
+
 import CardProduct from '../components/CardProduct.vue';
 import CardUser from '../components/CardUser.vue';
 import Table from '../components/Table.vue';
 import Login from '../components/Login.vue';
 import Loading from '../components/Loading.vue';
+import Modal from '../components/Modal.vue';
 
 export default {
   name: 'Tienda',
@@ -83,11 +95,34 @@ export default {
     Table,
     Login,
     Loading,
-    CardUser
+    CardUser,
+    Modal,
+    vSelect,
+    VueDatePicker,
   },
   data() {
     return {
+      date: null,
+      select: null,
       isLoading: false,
+      products: [
+        {
+          img: 'https://avatars.githubusercontent.com/u/60611243?v=4',
+          name: 'title',
+          description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+          stock: 10,
+          reference: 'asd1234',
+          price: 1000,
+        },
+        {
+          //img: 'https://avatars.githubusercontent.com/u/60611243?v=4',
+          name: 'title',
+          description: 'Some quick example text to build on the card title and make up the bulk of the card\'s content.',
+          stock: 10,
+          reference: 'asd1234',
+          price: 1000,
+        }
+      ],
       columns: {
         accion: {
           label: 'acciones',
@@ -151,7 +186,6 @@ export default {
       ]
     }
   },
-  
   async mounted() {
     document.title = 'Tienda';
     this.cargartooltipsbootstrap()
@@ -161,9 +195,13 @@ export default {
       const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]');
       [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl));
     },
-    login(nickname, password){
+    login(nickname, password) {
       this.isLoading = true;
       console.log('llega', nickname, password);
+    },
+    metoxxx(value) {
+      this.$refs.modal.show()
+      console.log('asd', this.select, value);
     }
   },
 }
